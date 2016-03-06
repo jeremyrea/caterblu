4ff049c29067da75b8226dedaeaffa004b3675f4
Jeremy Rea <jeremyrea@fastmail.com>
2016-03-05 21:56:54 -0500

    Fix form validation, page will load when search field is empty

--- source/views.py
+++ source/views.py
@@ -1,22 +1,17 @@
-import requests
-import os
-
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render_to_response
 from .forms import SearchForm
 from source import view_models
 
 
 # Create your views here.
 def index(request):
-    # return HttpResponse(controllers.get_bluray_rating())
-    # return render(request, 'index.html')
     if request.method == 'GET':
-        form = SearchForm(initial=request.GET.dict())
+        form = SearchForm(request.GET)
 
+        if form.is_valid():
+            return render(request, 'index.html', {'form': form, 'rt_rating': view_models.get_rt_rating(request.GET.__getitem__('movie_title').__str__()), 'bluray_rating': view_models.get_bluray_rating(request.GET.__getitem__('movie_title').__str__()), 'tech_specs': view_models.get_tech_spec(request.GET.__getitem__('movie_title').__str__())})
 
-        return render(request, 'index.html', {'form': form, 'rt_rating': view_models.get_rt_rating(request.GET.__getitem__('movie_title').__str__()), 'bluray_rating': view_models.get_bluray_rating(request.GET.__getitem__('movie_title').__str__()), 'tech_specs': view_models.get_tech_spec(request.GET.__getitem__('movie_title').__str__())})
     else:
         form = SearchForm()
 
