63fd93674899c62fb6d7a8ba0b0bee86e22351bb
Jeremy Rea <jeremyrea@fastmail.com>
2016-03-06 20:01:21 -0500

    Added thread pool to get different informations asynchoniously, should cut page load time in half on average

--- source/views.py
+++ source/views.py
@@ -1,3 +1,4 @@
+from multiprocessing.pool import ThreadPool
 from django.shortcuts import render
 from .forms import SearchForm
 from source import view_models
@@ -9,11 +10,19 @@ def index(request):
 
         if form.is_valid():
             title = request.GET.__getitem__('movie_title').__str__()
-            rt_rating = view_models.get_rt_rating(title)
-            bluray_rating = view_models.get_bluray_rating(title)
-            tech_specs = view_models.get_tech_spec(title)
-            price = view_models.get_price(title)
-            artwork = view_models.get_artwork(title)
+            
+            pool = ThreadPool(processes=5)
+            async_rt_rating = pool.apply_async(view_models.get_rt_rating, (title,))
+            async_bluray_rating = pool.apply_async(view_models.get_bluray_rating, (title,))
+            async_tech_specs = pool.apply_async(view_models.get_tech_spec, (title,))
+            async_price = pool.apply_async(view_models.get_price, (title,))
+            async_artwork = pool.apply_async(view_models.get_artwork, (title,))
+            
+            rt_rating = async_rt_rating.get()
+            bluray_rating = async_bluray_rating.get()
+            tech_specs = async_tech_specs.get()
+            price = async_price.get()
+            artwork = async_artwork.get()
 
             return render(request, 'index.html', {'form': form, 'rt_rating': rt_rating, 'bluray_rating': bluray_rating, 'tech_specs': tech_specs, 'price': price, 'artwork': artwork})
 
