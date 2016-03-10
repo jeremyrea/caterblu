81564b1113934631b36cc75da04148e7771c3f39
Jeremy Rea <jeremyrea@fastmail.com>
2016-03-09 21:10:34 -0500

    Remove unused imports

--- source/view_models.py
+++ source/view_models.py
@@ -2,7 +2,6 @@ import requests
 import re
 import os
 import itunespy
-import json
 
 from amazon.api import AmazonAPI
 from bs4 import BeautifulSoup
@@ -123,20 +122,20 @@ def get_price(title):
 def get_artwork(title):
     movies = itunespy.search_movie(title)
     movie_id = movies[0].track_id
-    
+
     url = 'https://itunes.apple.com/lookup?'
     payload = {'id': str(movie_id)}
-    
+
     '''Can also set country by adding
             &country=$COUNTRY
        in payload
     '''
-    
+
     response = requests.post(url, data=payload)
     data = response.json()
     image_address = data['results'][0]['artworkUrl100']
     # Does not work in Chrome
     #image_address = image_address.replace('http', 'https')
     image_address = image_address.replace('100x100', '500x500')
-    
-    return image_address    
+
+    return image_address
