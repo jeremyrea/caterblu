0564c50d7a7bb469612a29b6cbf68ea76b991d4d
Jeremy Rea <jeremyrea@fastmail.com>
2016-03-11 13:10:58 -0500

    Reorder import statements for consistency

--- caterblu/urls.py
+++ caterblu/urls.py
@@ -1,14 +1,10 @@
+import source.views.views
+
 from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 from django.contrib import admin
 admin.autodiscover()
 
-import source.views.views
-
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
     url(r'^$', source.views.views.index, name='index'),
