f1ff155a5c3f6a71f30365f1f5d5ffc022fc0efc
Jeremy Rea <jeremyrea@fastmail.com>
2016-03-26 20:17:21 -0400

    Use new STATICFILES_STORAGE value for latest whitenoise 3.x

--- caterblu/settings.py
+++ caterblu/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = (
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICTILES_FINDER = 'djangobower.finders.BowerFinder'
 BOWER_COMPONENTS_ROOT = os.path.join(PROJECT_ROOT, '')
