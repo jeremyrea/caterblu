974dd466d14cb89e6dbad38f6f7a518d60c25cc7
Jeremy Rea <jeremyrea@fastmail.com>
2016-04-26 12:24:48 -0400

    Resolves issue #5 - Remove "The " from title for RT if it doesn't work at first

--- source/services/rotten_tomatoes_service.py
+++ source/services/rotten_tomatoes_service.py
@@ -11,11 +11,18 @@ class RottenTomatoesService:
 
     def __init__(self, title):
         self.title = title
+        self.formatted_title = title
 
     def get_rt_rating(self):
-        search_url = self.__URL + self.format_title()
-
+        self.format_title()
+        search_url = self.__URL + self.formatted_title
         movie_page = requests.get(search_url)
+
+        if 404 == movie_page.status_code:
+            self.format_title_second_pass()
+            search_url = self.__URL + self.formatted_title
+            movie_page = requests.get(search_url)
+
         contents = movie_page.text
         soup = BeautifulSoup(contents, 'lxml')
 
@@ -25,23 +32,23 @@ class RottenTomatoesService:
         return ratings
 
     def format_title(self):
-        formatted_title = self.title
+        if self.formatted_title.startswith('A '):
+            self.formatted_title = self.formatted_title.replace('A ', '', 1)
+        if "'s" in self.formatted_title:
+            self.formatted_title = self.formatted_title.replace("'s", 's')
 
-        if formatted_title.startswith('The '):
-            formatted_title = formatted_title.replace('The ', '', 1)
-        if formatted_title.startswith('A '):
-            formatted_title = formatted_title.replace('A ', '', 1)
-        if "'s" in formatted_title:
-            formatted_title = formatted_title.replace("'s", 's')
+        self.formatted_title = self.formatted_title.replace(' ', self.__SEPERATOR)
+        self.formatted_title = self.formatted_title.replace('-', '')
+        self.formatted_title = self.formatted_title.replace(':', '')
+        self.formatted_title = self.formatted_title.replace(',', '')
+        self.formatted_title = self.formatted_title.replace('.', '')
+        self.formatted_title = self.formatted_title.replace('&', 'and')
 
-        formatted_title = formatted_title.replace(' ', self.__SEPERATOR)
-        formatted_title = formatted_title.replace('-', '')
-        formatted_title = formatted_title.replace(':', '')
-        formatted_title = formatted_title.replace(',', '')
-        formatted_title = formatted_title.replace('.', '')
-        formatted_title = formatted_title.replace('&', 'and')
 
-        return formatted_title
+    def format_title_second_pass(self):
+        if self.formatted_title.startswith('The_'):
+            self.formatted_title = self.formatted_title.replace('The_', '', 1)
+
 
     def get_ratings(self, soup):
         items = []
