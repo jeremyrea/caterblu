72d65a50d31fc32fadccc907c91c8e66ad192beb
Jeremy Rea <jeremyrea@fastmail.com>
2016-04-28 10:20:35 -0400

    Hide text input placeholder when onfocus

--- source/forms/search_form.py
+++ source/forms/search_form.py
@@ -5,5 +5,5 @@ from django_countries.fields import LazyTypedChoiceField
 
 
 class SearchForm(forms.Form):
-    title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Movie Title'}), max_length=150)
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'Movie Title', 'onfocus': 'this.placeholder = ""', 'onblur': 'this.placeholder = "Movie Title"'}), max_length=150)
     country = LazyTypedChoiceField(choices=django_countries.countries)
